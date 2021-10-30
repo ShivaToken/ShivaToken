c743fd9c38ea73e2470123b2932f660bac1e7734
Krypto-dev <86320602+krypto-dev@users.noreply.github.com>
2021-10-31 01:59:16 +1000

    Update and rename finalshiva.sol to SHIVA_V2(final).sol

--- SHIVA_V2(final).sol
+++ SHIVA_V2(final).sol
@@ -1068,7 +1068,7 @@ contract DividendPayingToken is ERC20, Ownable, DividendPayingTokenInterface, Di
   using SafeMathInt for int256;
 
   address public constant BTCB = address(0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c); //BTCB
-  // for tesnet: 0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8
+  // for testnet: 0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8
   // for mainnet: 0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c
 
   // With `magnitude`, we can properly distribute dividends even if the amount of received ether is small.
@@ -1397,69 +1397,10 @@ library Address {
     }
 }
 
-// File: @openzeppelin/contracts/security/ReentrancyGuard.sol
 
-/**
- * @dev Contract module that helps prevent reentrant calls to a function.
- *
- * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
- * available, which can be applied to functions to make sure there are no nested
- * (reentrant) calls to them.
- *
- * Note that because there is a single `nonReentrant` guard, functions marked as
- * `nonReentrant` may not call one another. This can be worked around by making
- * those functions `private`, and then adding `external` `nonReentrant` entry
- * points to them.
- *
- * TIP: If you would like to learn more about reentrancy and alternative ways
- * to protect against it, check out our blog post
- * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
- */
-abstract contract ReentrancyGuard {
-    // Booleans are more expensive than uint256 or any type that takes up a full
-    // word because each write operation emits an extra SLOAD to first read the
-    // slot's contents, replace the bits taken up by the boolean, and then write
-    // back. This is the compiler's defense against contract upgrades and
-    // pointer aliasing, and it cannot be disabled.
-
-    // The values being non-zero value makes deployment a bit more expensive,
-    // but in exchange the refund on every call to nonReentrant will be lower in
-    // amount. Since refunds are capped to a percentage of the total
-    // transaction's gas, it is best to keep them low in cases like this one, to
-    // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
-
-    uint256 private _status;
-
-    constructor() public {
-        _status = _NOT_ENTERED;
-    }
-
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
-     * `private` function that does the actual work.
-     */
-    modifier nonReentrant() {
-        // On the first call to nonReentrant, _notEntered will be true
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        // Any calls to nonReentrant after this point will fail
-        _status = _ENTERED;
-
-        _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
-        _status = _NOT_ENTERED;
-    }
-}
 
 /* SHIVA.sol */
-contract SHIVA is ERC20, Ownable, ReentrancyGuard {
+contract SHIVA is ERC20, Ownable {
     using SafeMath for uint256;
     using Address for address payable;
 
@@ -1504,7 +1445,7 @@ contract SHIVA is ERC20, Ownable, ReentrancyGuard {
 	// limit swap enabled
     bool public limitSwap = true;
     // swap, liquify, dividend disabled
-    bool public swapAndLiquifyDividendEnabled = true;
+    bool public swapAndLiquifyDividendEnabled = false;
     // swap start block
     uint256 public swapStartblock = 99999999;
 	// Minimum time between 2 swap of an user (by the number of blocks)
@@ -1831,7 +1772,7 @@ contract SHIVA is ERC20, Ownable, ReentrancyGuard {
         address from,
         address to,
         uint256 amount
-    ) internal virtual override antiWhale(from, to, amount) nonReentrant {
+    ) internal virtual override antiWhale(from, to, amount) {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
 
